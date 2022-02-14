93427819a05c7115ce2a47f093b97325934fb429
Raphel Oliveira <raphael.oliveira@valemobi.com.br>
2022-02-13 23:13:10 -0300

    Create network environment enum options

---
+++ ListMovies/Infrastructure/Networking/Enviroment/NetworkEnvironment.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkEnvironment.swift
+//  ListMovies
+//
+//  Created by Raphael Oliveira on 2/13/22.
+//
+
+enum NetworkEnvironment {
+    case dev
+    case production
+    case stage
+}
